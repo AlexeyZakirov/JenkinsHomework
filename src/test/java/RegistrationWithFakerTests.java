21d649c8faa9c4eb0942bbfa1171fcd1e86f5cc8
AlexeyZakirov <tinwhipcf@gmail.com>
2024-10-13 18:47:13 +0900

    added attachments

---
+++ src/test/java/RegistrationWithFakerTests.java
@@ -0,0 +1,127 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import pages.RegistrationPage;
+import utils.RandomUtils;
+
+import static io.qameta.allure.Allure.step;
+
+
+public class RegistrationWithFakerTests extends TestBase {
+    RegistrationPage registrationPage = new RegistrationPage();
+    RandomUtils randomUtils = new RandomUtils();
+
+    String firstName = randomUtils.getFirstName();
+    String lastName = randomUtils.getLastName();
+    String email = randomUtils.getEmail();
+    String phoneNumber = randomUtils.getNumber();
+    String studentGender = randomUtils.getStudentGender();
+    String month = randomUtils.getMonth();
+    String year = randomUtils.getYear();
+    int day = randomUtils.getDay();
+    String subject = randomUtils.getSubject();
+    String hobby = randomUtils.getHobbies();
+    String pathToPicture = randomUtils.getImage();
+    String currentAddress = randomUtils.getAddress();
+    String state = randomUtils.getState();
+    String city = randomUtils.getCity(state);
+    String resultName = randomUtils.getResultName(firstName, lastName);
+    String resultBirthday = randomUtils.getResultBirthday(year, month, day);
+    String resultStateAndCity = randomUtils.getResultStateAndCity(state, city);
+    String invalidStudentNumber = randomUtils.getInvalidNumber();
+
+
+    @DisplayName("Заполнение всех полей при регистрации")
+    @Test
+    public void fillAllFieldsTest() {
+        step("Открыть страницу регистрации", () -> {
+            registrationPage.openPage();
+        });
+
+        step("Заполнить все поля на странице", () -> {
+            registrationPage.setStudentName(firstName, lastName)
+                    .setEmail(email)
+                    .setStudentGenderWrapper(studentGender)
+                    .setStudentNumber(phoneNumber)
+                    .setDateOfBirth(month, year, day)
+                    .setSubjects(subject)
+                    .setHobby(hobby)
+                    .uploadPicture(pathToPicture)
+                    .setCurrentAddressInput(currentAddress)
+                    .setStateAndCity(state, city);
+        });
+
+        step("Подтвердить регистрацию", () -> {
+            registrationPage.submitRegistration();
+        });
+
+        step("Проверить соответствие введенных значений в таблице результатов с введенными при регистрации",
+                () -> {
+                    registrationPage.checkResultDialogIsAppear()
+                            .checkRegistrationResult("Student Name", resultName)
+                            .checkRegistrationResult("Student Email", email)
+                            .checkRegistrationResult("Gender", studentGender)
+                            .checkRegistrationResult("Mobile", phoneNumber)
+                            .checkRegistrationResult("Date of Birth", resultBirthday)
+                            .checkRegistrationResult("Subjects", subject)
+                            .checkRegistrationResult("Hobbies", hobby)
+                            .checkRegistrationResult("Picture", pathToPicture)
+                            .checkRegistrationResult("Address", currentAddress)
+                            .checkRegistrationResult("State and City", resultStateAndCity);
+                });
+    }
+
+    @DisplayName("Заполнение только обязательных полей при регистрации")
+    @Test
+    public void fillMinimumFieldsTest() {
+        step("Открыть страницу регистрации", () -> {
+            registrationPage.openPage();
+        });
+
+        step("Заполнить только обязательные поля", () -> {
+            registrationPage.setStudentName(firstName, lastName)
+                    .setEmail(email)
+                    .setStudentGenderWrapper(studentGender)
+                    .setStudentNumber(phoneNumber)
+                    .setDateOfBirth(month, year, day);
+        });
+
+        step("Подтвердить регистрацию", () -> {
+            registrationPage.submitRegistration();
+        });
+
+        step("Проверить соответствие введенных значений в таблице результатов с введенными при регистрации",
+                () -> {
+                    registrationPage.checkResultDialogIsAppear()
+                            .checkRegistrationResult("Student Name", resultName)
+                            .checkRegistrationResult("Student Email", email)
+                            .checkRegistrationResult("Gender", studentGender)
+                            .checkRegistrationResult("Mobile", phoneNumber)
+                            .checkRegistrationResult("Date of Birth", resultBirthday);
+                });
+    }
+
+    @DisplayName("Ввод нечислового значения номера телефона при заполнении обязательных полей регистрации")
+    @Test
+    public void fillMinimumFieldsWithNonNumericNumberTest() {
+        step("Открыть страницу регистрации", () -> {
+            registrationPage.openPage();
+        });
+
+        step("Заполнить только обязательные поля, в поле ввода номера телефона ввести нечисловое значение",
+                () -> {
+                    registrationPage.setStudentName(firstName, lastName)
+                            .setEmail(email)
+                            .setStudentGenderWrapper(studentGender)
+                            .setStudentNumber(invalidStudentNumber)
+                            .setDateOfBirth(month, year, day);
+                });
+
+        step("Подтвердить регистрацию", () -> {
+            registrationPage.submitRegistration();
+        });
+
+        step("Проверить, что таблица результатов не отобразилась", () -> {
+            registrationPage.checkResultIsNotAppear();
+        });
+    }
+}
---
+++ src/test/java/TestBase.java
@@ -0,0 +1,42 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
+public class TestBase {
+    @BeforeAll
+    static void setUp() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterEach
+    void afterEach() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+
+        Selenide.closeWebDriver();
+    }
+}
